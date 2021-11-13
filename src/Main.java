30d5fffbeb45d951245d687173181ca8daae67c0
stefg2002 <86495577+stefg2002@users.noreply.github.com>
2021-11-13 00:49:36 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 import javax.swing.*;
 import java.util.*;
 
-/**
- * Stefano Giammarresi and Mathusan Mathyalagan
- * ICS 4U1
- * Ms. Gheorghiade
- * Jan 6 2020
- * A program that creates a database of science quotes. User is able to add, remove, display and search for quotes. They can also sort buy author and find the first quote by an author
- */
-
 public class Main extends JFrame{ //Extends JFrame class, used for creating a window
 	
 	private static final long serialVersionUID = 1L;
